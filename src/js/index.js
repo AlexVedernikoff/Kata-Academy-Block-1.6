d9834895ea32759d56862b5c79871f589cf312bd
Alex Vedernikov <100806495+AlexVedernikoff@users.noreply.github.com>
2022-04-29 06:07:47 +0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import '../scss/style.scss';
+
+import './buttons_modal.js';
+import './index_button.js';
+import './repairs_button.js';
+import './repairs_swiper.js';
+import './services_swiper.js';
+import './trademarks_button.js';
+import './trademarks_swiper.js';
+import './scroll_swiper.js';
+
+
+
